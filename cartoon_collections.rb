c4c0856127b5e2bb4057f8e6cd59a429342563ca
Will Andre <wandre94@gmail.com>
2020-12-30 00:35:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.all? { |call| call <= 4  }
+  calls.any? { |call| call > 4  }
 end
 
 def find_the_cheese# code an argument here
