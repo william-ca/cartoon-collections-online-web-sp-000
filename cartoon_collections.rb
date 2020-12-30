3b6a8f84de05d838b963d4129af585ad4bee6cd7
Will Andre <wandre94@gmail.com>
2020-12-30 00:41:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheeses)
-  cheeses.include(cheese_types)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheeses.include(cheese_types)
 end
